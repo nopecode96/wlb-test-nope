4e3e4438db0e085dfbedef8fc5822bd4f9c5f973
Nope Lavanda <nopecode96@gmail.com>
2021-02-22 02:26:24 +0700

    second

--- server.js
+++ server.js
@@ -1,47 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const md5 = require("md5");
 
-const app = express();
+const server = express();
 
-// Add headers
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
-
-
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-//
-// app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(md5);
-
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.sequelize.sync();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome." });
+server.get("/", (req, res) => {
+    res.json({ message: "Welcome." });
 });
 
-require("./app/routes/articles.routes")(app);
-require("./app/routes/comments.routes")(app);
-require("./app/routes/users.routes")(app);
-require("./app/routes/auth.routes")(app);
+require("./app/routes/articles.routes")(server);
+require("./app/routes/comments.routes")(server);
+require("./app/routes/users.routes")(server);
+require("./app/routes/auth.routes")(server);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+const PORT = process.env.PORT || 9696;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
